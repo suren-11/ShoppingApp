96ef4988a8a73c341251a617e0446ef8084240d8
suren-11 <presanth11@gmail.com>
2024-04-17 00:26:30 +0530

    First commit

---
+++ app/src/main/java/com/suren/shoppingapp/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.suren.shoppingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suren.shoppingapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/com/suren/shoppingapp/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.suren.shoppingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suren.shoppingapp.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
